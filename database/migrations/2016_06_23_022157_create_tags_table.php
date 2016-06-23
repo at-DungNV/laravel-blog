f823ce5c0d7cc5ff777b500df6c0af84834a7dc0
Dung Nguyen Van <nobinguyen94@gmail.com>
2016-06-23 10:20:31 +0700

    first commit

---
+++ database/migrations/2016_06_23_022157_create_tags_table.php
@@ -0,0 +1,32 @@
+<?php2016
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tags');
+    }
+}
